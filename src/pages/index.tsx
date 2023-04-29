23b788a9e45a826482473a1c661fc169e10296c6
Joel <58995538+jtpotato@users.noreply.github.com>
2023-04-29 13:41:19 +1000

    add blank option

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
 import Background from "@/components/background";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const router = useRouter();
+  const [blank, setBlank] = useState(true);
+
+  useEffect(() => {
+    console.log(window?.location.search)
+    if (!window?.location.search.includes("blank")) {
+      console.log("Not blank");
+      setBlank(false);
+    }
+  }, [])
+
   return (
     <div className="overflow-none">
       <Background spheres={20} />
       <div className="flex items-center justify-center w-screen h-screen text-white">
-        <h1 contentEditable className="text-7xl font-semibold outline-none">
-          Insert Title Here
-        </h1>
+        {blank ? (
+          <></>
+        ) : (
+          <h1 contentEditable className="text-7xl font-semibold outline-none">
+            Insert Title Here
+          </h1>
+        )}
       </div>
     </div>
   );
